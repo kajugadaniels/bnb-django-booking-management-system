218b9140b52d2c90af327ef60ff523fcf5abfd3d
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-23 19:35:09 +0200

    Routes for auth

--- account/urls.py
+++ account/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 app_name = 'auth'
 
 urlpatterns = [
-    # path('login/', user_login, name="login"),
-    # path('register/', user_register, name="register"),
-    # path('logout/', user_logout, name='logout'),
+    path('login/', user_login, name="login"),
+    path('register/', user_register, name="register"),
+    path('logout/', user_logout, name='logout'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
