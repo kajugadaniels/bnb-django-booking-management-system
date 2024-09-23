7753ae3af3f51d601ce6ff116021bda34b9f4814
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-23 20:13:04 +0200

    Register Room and RoomImage in admin.py

--- home/admin.py
+++ home/admin.py
@@ -5,3 +5,21 @@ from django.contrib import admin
 class AmenityAdmin(admin.ModelAdmin):
     list_display = ('name',)
     search_fields = ('name',)
+
+class RoomImageInline(admin.TabularInline):
+    model = RoomImage
+    extra = 1
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'price', 'capacity', 'created_at', 'updated_at')
+    search_fields = ('name', 'description')
+    list_filter = ('created_at', 'updated_at')
+    prepopulated_fields = {'slug': ('name',)}
+    filter_horizontal = ('amenities',)
+    inlines = [RoomImageInline]
+
+@admin.register(RoomImage)
+class RoomImageAdmin(admin.ModelAdmin):
+    list_display = ('room', 'created_at')
+    search_fields = ('room__name',)
