8c2fa0a8fd8606f2fcdc9e7c64b27ce6ed35953c
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-24 11:17:52 +0200

    Add function for adding reviews

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
+from home.forms import *
 from home.models import *
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 
 def home(request):
     rooms = Room.objects.prefetch_related('images').all()
@@ -28,9 +29,20 @@ def rooms(request):
 
 def getRoom(request, slug):
     room = get_object_or_404(Room, slug=slug)
+    
+    if request.method == 'POST':
+        form = RoomReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.room = room
+            review.save()
+            return redirect('home:getRoom', slug=room.slug)
+    else:
+        form = RoomReviewForm()
 
     context = {
         'room': room,
+        'form': form,
     }
 
     return render(request, 'rooms/show.html', context)
