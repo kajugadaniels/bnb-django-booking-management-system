7145162e9c962045d1e34e5eec85b778bb478c06
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-24 11:16:05 +0200

    Create RoomReviewForm

---
+++ home/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from home.models import *
+
+class RoomReviewForm(forms.ModelForm):
+    class Meta:
+        model = RoomReview
+        fields = ['location', 'staff', 'cleanliness', 'value_for_money', 'comfort', 'facilities', 'free_wifi', 'text', 'email', 'comment']
+
+        widgets = {
+            'location': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'staff': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'cleanliness': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'value_for_money': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'comfort': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'facilities': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'free_wifi': forms.NumberInput(attrs={'min': 1, 'max': 5}),
+            'text': forms.TextInput(attrs={'placeholder': 'Your name'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Your email'}),
+            'comment': forms.Textarea(attrs={'rows': 4, 'placeholder': 'Write your comment'}),
+        }
