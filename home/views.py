2102afeb3bcfe82833f0649fff606ab345edd419
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-26 11:36:16 +0200

    Fix issues

--- home/views.py
+++ home/views.py
@@ -22,7 +22,7 @@ def home(request):
     return render(request, 'index.html', context)
 
 def about(request):
-    return render(request, 'rooms/about.html')
+    return render(request, 'about.html')
 
 def rooms(request):
     rooms = Room.objects.prefetch_related('images').order_by('-created_at').all()
