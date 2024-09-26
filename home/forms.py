461deca8b2a37c7e4e7f7504e7db3d445ecffd2b
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-09-26 15:30:02 +0200

    Add ContactForm

--- home/forms.py
+++ home/forms.py
@@ -35,3 +35,14 @@ class BookingForm(forms.ModelForm):
     name = forms.CharField(max_length=100)
     email = forms.EmailField()
     phone = forms.CharField(max_length=15)
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'subject', 'message']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Your Name'}),
+            'email': forms.EmailInput(attrs={'class': 'form-input', 'placeholder': 'Your Email'}),
+            'subject': forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Subject'}),
+            'message': forms.Textarea(attrs={'class': 'form-input', 'placeholder': 'Your Message', 'rows': 4}),
+        }
