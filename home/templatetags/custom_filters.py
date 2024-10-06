3266a4877cb4da858a24330413a65fd1858e1aba
KAJUGA Daniels <kajugadaniels@gmail.com>
2024-10-06 18:46:56 +0200

    New changes

--- home/templatetags/custom_filters.py
+++ home/templatetags/custom_filters.py
@@ -4,4 +4,10 @@ register = template.Library()
 
 @register.filter
 def times(number):
+    """Returns a range of numbers to simulate Python's range() in templates."""
     return range(number)
+
+@register.filter
+def to_int(value):
+    """Converts a float or string value to an integer."""
+    return int(float(value))
