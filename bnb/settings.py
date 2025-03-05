38ecf19b7318cb07152b796f32f360858ac4525d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-03-05 16:02:40 +0200

    New hcnages

--- bnb/settings.py
+++ bnb/settings.py
@@ -77,26 +77,14 @@ WSGI_APPLICATION = 'bnb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if str(os.getenv("NODE_ENV"))=="production":
-
-    DATABASES =  {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.getenv("POSTGRES_DB"),      
-            'USER': os.getenv("POSTGRES_USER"),       
-            'PASSWORD': os.getenv("POSTGRES_PASSWORD"), 
-            'HOST': os.getenv("PGHOST"),         
-            'PORT':os.getenv("PGPORT"), 
-            'OPTIONS': {
-                'sslmode': 'require',
-            }            
-        }
-    }
-else:
-    DATABASES = {
+DATABASES =  {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv("MYSQL_DB", "bnb"),
+        'USER': os.getenv("MYSQL_USER", "bnbuser"),
+        'PASSWORD': os.getenv("MYSQL_PASSWORD", "S3cur3P@ssw0rd!"),
+        'HOST': os.getenv("MYSQL_HOST", "vmi2421856.contaboserver.net"),
+        'PORT': os.getenv("MYSQL_PORT", "3306"),
     }
 }
 
