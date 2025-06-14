4d5b704ea392fab1ec5099dac72aa092dd3e2ce3
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-14 18:26:48 +0200

    New changes

--- bnb/settings.py
+++ bnb/settings.py
@@ -77,17 +77,23 @@ WSGI_APPLICATION = 'bnb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES =  {
+# DATABASES =  {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.getenv("MYSQL_DB", "bnb"),
+#         'USER': os.getenv("MYSQL_USER", "bnbuser"),
+#         'PASSWORD': os.getenv("MYSQL_PASSWORD", "S3cur3P@ssw0rd!"),
+#         'HOST': os.getenv("MYSQL_HOST", "vmi2421856.contaboserver.net"),
+#         'PORT': os.getenv("MYSQL_PORT", "3306"),
+#     }
+# }
+
+DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("MYSQL_DB", "bnb"),
-        'USER': os.getenv("MYSQL_USER", "bnbuser"),
-        'PASSWORD': os.getenv("MYSQL_PASSWORD", "S3cur3P@ssw0rd!"),
-        'HOST': os.getenv("MYSQL_HOST", "vmi2421856.contaboserver.net"),
-        'PORT': os.getenv("MYSQL_PORT", "3306"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
