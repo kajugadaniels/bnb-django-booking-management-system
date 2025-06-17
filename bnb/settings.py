a028935f3d42a9bed91d39b52216a46f2f56c67d
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-17 23:08:39 +0200

    New changes

--- bnb/settings.py
+++ bnb/settings.py
@@ -216,8 +216,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', "nexcoderwa@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', "fbdcommddznrgtyy")
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', "bandbmountainview@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', "kwfrftrimsoamdio")
 # DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'Your App <no-reply@yourapp.com>')
 
-CONTACT_EMAIL = 'nexcoderwa@gmail.com'
+CONTACT_EMAIL = 'bandbmountainview@gmail.com'
