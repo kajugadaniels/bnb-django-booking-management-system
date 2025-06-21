20cebc9d4926c7a60224780d784d0d770b2c8cc0
KAJUGA Daniels <163526843+kajugadaniels@users.noreply.github.com>
2025-06-21 15:29:50 +0200

    Update settings.py

--- bnb/settings.py
+++ bnb/settings.py
@@ -77,21 +77,14 @@ WSGI_APPLICATION = 'bnb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES =  {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': os.getenv("MYSQL_DB", "bnb"),
-#         'USER': os.getenv("MYSQL_USER", "bnbuser"),
-#         'PASSWORD': os.getenv("MYSQL_PASSWORD", "S3cur3P@ssw0rd!"),
-#         'HOST': os.getenv("MYSQL_HOST", "vmi2421856.contaboserver.net"),
-#         'PORT': os.getenv("MYSQL_PORT", "3306"),
-#     }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASES =  {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': os.getenv("MYSQL_DB", "bnb"),
+         'USER': os.getenv("MYSQL_USER", "bnbuser"),
+         'PASSWORD': os.getenv("MYSQL_PASSWORD", "S3cur3P@ssw0rd!"),
+         'HOST': os.getenv("MYSQL_HOST", "vmi2421856.contaboserver.net"),
+         'PORT': os.getenv("MYSQL_PORT", "3306"),
     }
 }
 
